a76948f382aa0ca6497ad9fbdba28c8b7da1d4ee
Keaton Nordby <keatonnordby4@gmail.com>
2025-12-20 12:18:34 -0600

    fixed key issue / master password

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -1,32 +1,50 @@
-# simple password manager that allows you to view and add passwords
-# includes simple "encryption"
+from cryptography.fernet import Fernet
 
-master_pwd = input("What is your master password? ")
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
 
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print(user, passw)
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
 
 
 def add():
-    name = input("Account Name: ")
+    name = input('Account Name: ')
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + " | " + pwd + "\n")
-        
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
 
 while True:
-    mode = input("Would you like to add a new password or view existing ones (view, add), press (q) to quit? ")
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
     if mode == "q":
         break
+
     if mode == "view":
         view()
     elif mode == "add":
         add()
     else:
-        print("Invalid code")
+        print("Invalid mode.")
         continue
